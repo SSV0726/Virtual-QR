e8f2e2221413aaf0b444333628a21547a6a4fa5c
Sagar <ssv0726@gmail.com>
2020-06-11 02:51:33 +0530

    Best till now

--- app/src/main/java/com/npci/virtualqr/splashscreen.java
+++ app/src/main/java/com/npci/virtualqr/splashscreen.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class splashscreen extends AppCompatActivity {
 
@@ -12,6 +15,9 @@ public class splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        ImageView rings = findViewById(R.id.broadcastingGif);
+        Glide.with(this).asGif().load(R.drawable.final_logo).into(rings);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
